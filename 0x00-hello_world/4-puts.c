b069f8323e8d7548715dc0d8244b107814537235
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-03 08:31:49 -0800

    Double quotes at the beginning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 	return (0);
