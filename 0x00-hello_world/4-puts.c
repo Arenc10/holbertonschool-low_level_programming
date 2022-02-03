eb27fd317bc92cbf2aef95ab4a175d2f459022cc
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-03 08:12:48 -0800

    Added a comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-int main(void)
+//A function that prints a string
+int main(void) 
 {
 	char Mystr[] = "Programming is like building a multilingual puzzle\n";
 
-	puts(Mystr);
-	
+	puts(Mystr);	
 	return (0);
 }
