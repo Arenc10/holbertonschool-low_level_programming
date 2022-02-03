532fcb767ada793da77a91e1baf731dccba1254e
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-03 08:38:40 -0800

    Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/*
- * main - A function that prints a string
- *
- * Return: Always 0 (Success)
- */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(Mystr);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
