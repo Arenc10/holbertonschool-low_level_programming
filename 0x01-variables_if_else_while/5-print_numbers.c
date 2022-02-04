5697c5d51f2ea93342db1edaaf4d56c05a4ea5cb
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-04 14:04:24 -0800

    Printing 10 based numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Printing all base ten numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	printf("%i", num);
+	putchar('\n');
+	return (0);
+}
