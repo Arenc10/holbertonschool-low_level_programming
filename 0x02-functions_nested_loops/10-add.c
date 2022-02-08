be0af64796d41c85a67a951bc8507cfe222b2721
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-08 15:07:20 -0800

    Adding two ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * add - A function that adds to integers
+ * @a: first integer to add
+ * @b: second integer to add
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
