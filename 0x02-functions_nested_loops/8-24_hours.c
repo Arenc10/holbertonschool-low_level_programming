d64ab805ef8b9ee56f90c3cb9b81dda713a71ecb
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-08 10:22:49 -0800

    Jack bauer minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - Function that prints every minute of the day
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i <= 2 && j <= 3)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(k + '0');
+						_putchar(l + '0');
+					}
+				}
+			}
+		}
+	}
+}
