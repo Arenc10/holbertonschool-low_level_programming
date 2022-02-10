6f768cdaa4391cdf2afa4d21e8d24a0122f085b1
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-10 05:39:54 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
 
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+}
