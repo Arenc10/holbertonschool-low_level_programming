9b5cd3dbc35167b6f2a8c2cbde9b5323774336c8
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-14 05:49:42 -0800

    Function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - Function that prints string
+ * @str: first operand a character
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int l = 0;
+	
+	while (str[l] != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
