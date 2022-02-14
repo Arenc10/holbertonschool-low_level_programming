7ab17c26c21e7cee6950c9dcbce54162b33900d9
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-14 07:34:34 -0800

    Function that opies to another point

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - function that copies the string to another point
+ * @dest: first operand a char
+ * @src: second operand a char
+ * Return: Always 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
