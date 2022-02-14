d4f55ea2252d23fe7a123737e14b4382d2ead410
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-14 05:21:02 -0800

    Convert number to *n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that updates the value
+ * @*n: First operand an pointer
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
