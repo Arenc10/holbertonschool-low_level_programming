646ad40522320fc571a3f4e91dc703b13fbb84a5
Arenc10 <arenc.palluqi@yahoo.com>
2022-02-14 07:20:40 -0800

    Function that prints an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Function that prints an array of integers
+ * @a: first operand an integer
+ * @n: second operand an integer
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0;i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n-1)
+			printf(", ");
+	}
+	_putchar('\n');
+}
