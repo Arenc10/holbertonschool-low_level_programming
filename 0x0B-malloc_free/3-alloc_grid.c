cce272f6753c46d3aa77c6660e1cbe2bba3b17a5
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-01 12:01:38 -0800

    Hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,11 +19,15 @@ int **alloc_grid(int width, int height)
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
-		arr[i] = malloc(sizeof(int) * width);
-	if (arr[i] == NULL)
 	{
-		for (; i >= 0; i--)
-			free(arr[i]);
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{	
+			for (; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
