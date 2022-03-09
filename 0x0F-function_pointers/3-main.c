664cbb65a7e26287be58f28b8bd82d7d802252ad
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-09 06:34:02 -0800

    A programt that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: First operand
+ * @argv: Second operand
+ * Return: Return 0 Always (Success)
+ */
+int main(int argc, int **argv)
+{
+	int num1;
+	int num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = atoi(argv[2]);
+
+	if (get_op_func(op) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' || *op == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	prtinf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_function.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - Function that adds
+ * @a: First operand
+ * @b: Second operand
+ * Return: Return an int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Function that subtracts
+ * @a: First operand
+ * @b: Second operand
+ * Return: Return an int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Function that multiplies
+ * @a: First operand
+ * @b: Second operand
+ * Return: Return an int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Function that divides
+ * @a: First operand
+ * @b: Second operand
+ * Return: Return an int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Function that shows the reaminder
+ * @a: First operand
+ * @b: second operand
+ * Return: Return an in
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
