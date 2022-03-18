9e7600666930ef5a2eecb757dae69b2209fba760
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-18 06:27:06 -0700

    Function called before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void quote(void) __attribute__ ((constructor));
+/**
+ * quote - Prints before main is called
+ */
+void quote(void) __attribute__ ((constructor))
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
