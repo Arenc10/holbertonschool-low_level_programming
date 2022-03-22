06985272d7c630dd3bb5c04e6aa970c70f651930
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-22 07:46:45 -0700

    Added @ at the comment

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 /**
  * get_nodeint_at_index - Function that returns the nth node of a linked list
  * @head: First operand pointer to listint_t
- * index: Second operand an unsigned int
+ * @index: Second operand an unsigned int
  * Return: Return the node or NULL if node doesent exist
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
