49f7c904ee140c70bee6fcfc7034e166f7d0b47a
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-24 05:55:37 -0700

    Fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,10 +8,12 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bitValue;
-	if (n > 1)
-	{
+	unsigned long int size = (sizeof(n) * 8);
+
+	if (index > size)
+		return (-1);
+	if (n > 0)
 		bitValue = (n >> index) & 1;
-		return (bitValue);
-	}
-	return (-1);
+		
+	return (bitValue);
 }
