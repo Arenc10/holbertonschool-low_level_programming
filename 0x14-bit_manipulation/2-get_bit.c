96bdf9c1e26a08ba055fe9f410d1dcb77db82ee9
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-24 05:40:31 -0700

    Function that returns the value of a bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit - Function that returns the value of a bit at a given index
+ * @n: First operand unsigned long int
+ * @index: Second operand unsigned int
+ * Return: Return the value of a bit at a given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bitValue;
+
+	bitValue = (n >> index) & 1;
+	return (bitValue);
+	if (n < 0)
+		return (-1);
+}
--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * main - check the code
- *  *
- *   * Return: Always 0.
- *    */
- int main(void)
+int main(void)
 {
-	    unsigned int n;
+	    int n;
 
-	        n = binary_to_uint("1");
-		    printf("%u\n", n);
-		        n = binary_to_uint("101");
-			    printf("%u\n", n);
-			        n = binary_to_uint("1e01");
-				    printf("%u\n", n);
-				        n = binary_to_uint("1100010");
-					    printf("%u\n", n);
-					        n = binary_to_uint("0000000000000000000110010010");
-						    printf("%u\n", n);
-						        return (0);
+	        n = get_bit(1024, 10);
+		    printf("%d\n", n);
+		        n = get_bit(98, 1);
+			    printf("%d\n", n);
+			        n = get_bit(1024, 0);
+				    printf("%d\n", n);
+				        return (0);
 }
