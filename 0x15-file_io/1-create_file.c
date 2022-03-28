c4678f8b990a5cbff9636a27b131f155c0ac7da6
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-28 04:43:35 -0700

    Text content fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,10 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, wr, i, length = 0;
-	
+
 	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
 		return (-1);
 	for (i = 0; text_content[i] != '\0'; i++)
 		length++;
