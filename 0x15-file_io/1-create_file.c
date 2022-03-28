c915fe8781731bd1d0ede661dce1decbdbd28fd9
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-28 04:28:28 -0700

    Function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_file - Function that creates a file
+ * @filename: First operand
+ * @text_content: Second operand
+ * Return: 1 on success or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, i, length = 0;
+
+	for (i = 0; text_content[i] != '\0'; i++)
+		length++;
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	if (fd == -1)
+		return (-1);
+	wr = write(fd, text_content, length);
+	if (wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
