967d76513e5cb4925a4b4775583ed47dcc8ac5fd
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-28 06:48:57 -0700

    Fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	wr = write(file, text_content, len);
+	wr = write(fd, text_content, len);
 	if (wr == -1)
 		return (-1);
 	close(fd);
