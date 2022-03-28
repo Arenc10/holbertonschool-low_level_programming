5c8241da6c843447be71df1d2a0b252c66be2ba9
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-28 04:46:15 -0700

    Betty fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
-	for (i = 0; text_content[i] != '\0'; i++)
-		length++;
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			length++;
+	}
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
