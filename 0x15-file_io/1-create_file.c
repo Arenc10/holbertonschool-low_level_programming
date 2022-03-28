b6d8c0394f421ab3330165f5f0e0bda1cec9698c
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-28 04:33:04 -0700

    Fixed permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	for (i = 0; text_content[i] != '\0'; i++)
 		length++;
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	wr = write(fd, text_content, length);
