7fa2cbe1abf1fbcd4d41585e2565c3808af3077e
Arenc10 <arenc.palluqi@yahoo.com>
2022-05-05 04:46:12 -0700

    Insert node at givend index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		if (temp == NULL)
-			return (NULL)
+			return (NULL);
 		idx--;
 	}
 	if (temp->next == NULL)
