4a1745ef5e87cda005902f91e7fa9b0d9212ba48
Arenc10 <arenc.palluqi@yahoo.com>
2022-05-27 10:43:04 -0700

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h;
-	unsigned long int i;
 
 	h = malloc(sizeof(hash_table_t));
 	if (h == NULL)
@@ -16,7 +15,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	h->array = malloc(sizeof(hash_node_t *) * size);
 	if (h->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		h->array[i] = NULL;
 	return (h);
 }
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,27 +1,32 @@
 #include "hash_tables.h"
 /**
- * hash_table_print - Function that prints a hash table
- * @ht: First operand
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i, j = 0;
 	hash_node_t *node;
-	unsigned long int i;
 
 	if (ht == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array != NULL)
-			node = ht->array[i];
-		while (node != NULL)
+		if (ht->array[i] != NULL)
 		{
-			printf("'%s': '%s'", node->key, node->value);
-			if (node != NULL)
+			if (j == 1)
 				printf(", ");
-			node = node->next;
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			j = 1;
 		}
 	}
 	printf("}\n");
-}	
+}
